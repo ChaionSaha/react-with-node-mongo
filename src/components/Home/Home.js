0129272b004e76160772a9976c6c527079efef7b
Chaion Saha <sahachaion2000@gmail.com>
2023-03-07 13:55:39 +0600

    delete user done

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,18 +1,26 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import User from '../User/User';
 import './Home.scss';
 
 const Home = () => {
 	const [users, setUsers] = useState([]);
-	fetch('http://localhost:5000/users')
-		.then((res) => res.json())
-		.then((data) => setUsers(data));
+	useEffect(() => {
+		fetch('http://localhost:5000/users')
+			.then((res) => res.json())
+			.then((data) => setUsers(data));
+	}, []);
+
 	return (
 		<div className='home'>
 			<h1>Total user: {users.length} </h1>
 			<div className='users'>
 				{users.map((user) => (
-					<User key={user._id} user={user}></User>
+					<User
+						key={user._id}
+						user={user}
+						allUsers={users}
+						setUsers={setUsers}
+					></User>
 				))}
 			</div>
 		</div>
--- src/components/User/User.js
+++ src/components/User/User.js
@@ -1,9 +1,26 @@
 import { PencilSquareIcon, TrashIcon } from '@heroicons/react/24/outline';
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { toast } from 'react-toastify';
 import './User.scss';
 
-const User = ({ user }) => {
+const User = ({ user, allUsers, setUsers }) => {
+	const handleUserDelete = (id) => {
+		console.log('deleting user', id);
+
+		fetch(`http://localhost:5000/userDelete/${id}`)
+			.then((res) => res.json())
+			.then((data) => {
+				if (data.deletedCount > 0) {
+					let updatedUsers = allUsers.filter(
+						(user) => user._id != id
+					);
+					setUsers(updatedUsers);
+					toast.success(`User: ${user.firstName} deleted`);
+				}
+			});
+	};
+
 	return (
 		<div className='user'>
 			<h1 className='name'>
@@ -15,7 +32,9 @@ const User = ({ user }) => {
 				<Link to={`/user/${user._id}`}>
 					<PencilSquareIcon></PencilSquareIcon>
 				</Link>
-				<TrashIcon></TrashIcon>
+				<TrashIcon
+					onClick={() => handleUserDelete(user._id)}
+				></TrashIcon>
 			</div>
 		</div>
 	);
